624bd5f5d3a3350b83dfd9e46f3ad5bc537734cb
Sebastian Klepper <sk@sebastianklepper.com>
2013-02-21 11:23:48 +0100

    Add TomDoc for ObjectStore.

--- lib/memstore.rb
+++ lib/memstore.rb
@@ -7,18 +7,22 @@ require "memstore/queries"
 
 module MemStore
 
+  # Shortcut to ObjectStore::new
   def self.new(key=nil, items={})
     ObjectStore.new(key, items)
   end
 
+  # Shortcut to ObjectStore::from_binary
   def self.from_binary(binary)
     ObjectStore.from_binary(binary)
   end
 
+  # Shortcut to ObjectStore::from_file
   def self.from_file(file)
     ObjectStore.from_file(file)
   end
 
+  # Shortcut to ObjectStore::with_file
   def self.with_file(file, key=nil, items={}, &block)
     ObjectStore.with_file(file, key, items, &block)
   end
